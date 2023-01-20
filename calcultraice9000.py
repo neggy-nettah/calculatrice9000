eb658674e56ee8bacb4af82fa0ddcc84116bc9c6
neggy-nettah <neggy.nettah@laplateforme.io>
2023-01-20 10:56:55 +0100

    finish

--- calcultraice9000.py
+++ calcultraice9000.py
@@ -36,6 +36,18 @@ label_dot = Button(text=".", width=5, height=3, font=10, bg="gray68", command=la
 label_square = Button(text="sqr", width=5, height=3, font=10, bg="grey44", command=lambda: button_square())
 label_percentage = Button(text="%", width=5, height=3, font=10, bg="dark orange", command=lambda: button_click("//"))
 label_squareroot = Button(text="sqrt", width=5, height=3, font=10, bg="dark orange", command=lambda: button_squareroot())
+root.bind(1, lambda event: button_click(1))
+root.bind(2, lambda event: button_click(2))
+root.bind(3, lambda event: button_click(3))
+root.bind(4, lambda event: button_click(4))
+root.bind(5, lambda event: button_click(5))
+root.bind(6, lambda event: button_click(6))
+root.bind(7, lambda event: button_click(7))
+root.bind(8, lambda event: button_click(8))
+root.bind(9, lambda event: button_click(9))
+root.bind(0, lambda event: button_click(0))
+
+
 
 #button fonction
 def button_click(number):
